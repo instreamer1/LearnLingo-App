602bc3dbdac4403ae1255569b34ee07eb4093ca6
Aleksander <aleksander.0705@gmail.com>
2024-11-24 12:39:01 +0200

    create vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -9,44 +9,10 @@ import store  from './redux/store.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter  future={{ v7_relativeSplatPath: true }}>
+    <BrowserRouter >
         <App />
       </BrowserRouter>
       </Provider>
     </React.StrictMode>
 )
 
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import "./index.css";
-// import App from "./App";
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
-
-// const router = createBrowserRouter(
-//   [
-//     {
-//       path: "/*",
-//       element: <App />,
-//     },
-//   ],
-//   {
-//     future: {
-//       v7_relativeSplatPath: true,
-//       v7_startTransition: true,
-//       v7_fetcherPersist: true,
-//       v7_normalizeFormMethod: true,
-//       v7_partialHydration: true,
-//       v7_skipActionErrorRevalidation: true,
-//     },
-//   }
-// );
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//       <RouterProvider router={router} />
-//     </Provider>
-//   </StrictMode>
-// );
